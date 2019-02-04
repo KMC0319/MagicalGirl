2dbaccb1edcf80b0efd4c2c0c32147f84e4a912a
KMC0319 <31245129+KMC0319@users.noreply.github.com>
2019-02-04 15:52:15 +0900

    BossInit

---
+++ PrettyMomoka/Assets/Scripts/Boss/BossController.cs
@@ -0,0 +1,45 @@
+﻿using Systems;
+using UnityEngine;
+
+namespace Boss {
+    public class BossController : MonoBehaviour, IHitAttack {
+        private enum BossState {
+            Wait, 
+        }
+        [SerializeField] private int hp;
+        private Camera mainCamera;
+        private BossState state;
+        private bool isActive;
+
+        void Start() {
+            mainCamera = Camera.main;
+        }
+
+        void Update() {
+            if (!isActive) {
+                isActive = IsInCamera();
+                if (isActive) Init();
+                return;
+            }
+        }
+
+        private void Init() {
+            state = BossState.Wait;
+        }
+
+        private bool IsInCamera() {
+            var rect = new Rect(0, 0, 1, 1);
+            return rect.Contains(mainCamera.WorldToViewportPoint(transform.position));
+        }
+        
+        private void Dead() {
+            Debug.Log("Boss is dead");
+        }
+        
+        public void Damage(int damage) {
+            hp -= damage;
+            if(hp <= 0) Dead();
+            
+        }
+    }
+}
---
+++ PrettyMomoka/Assets/Scripts/Systems/IHitAttack.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+namespace Systems {
+    public interface IHitAttack {
+        void Damage(int damage);
+    }
+}
