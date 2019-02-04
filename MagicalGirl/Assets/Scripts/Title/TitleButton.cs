b52a43838efab06c28c1abe29a0194574f69930a
KMC0319 <31245129+KMC0319@users.noreply.github.com>
2019-02-05 02:31:18 +0900

    fix name

---
+++ MagicalGirl/Assets/Scripts/Title/TitleButton.cs
@@ -0,0 +1,19 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Title {
+    public class TitleButton : MonoBehaviour {
+        public void OnClickStart() {
+            SceneManager.LoadScene("Game");
+        }
+
+        public void Quit() {
+#if UNITY_EDITOR
+            EditorApplication.isPlaying = false;
+#elif UNITY_STANDALONE
+      Application.Quit();
+    #endif
+        }
+    }
+}
